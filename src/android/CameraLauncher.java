8ab421e356f427e9cd604ebf130ea5affc187d03
Josef Kerkinni <jkerkinni@administorsair2.hs.internal>
2015-01-13 17:43:50 -0600

    no message

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -418,7 +418,7 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
                 }
 
                 // Add compressed version of captured image to returned media store Uri
-                LOG.e(LOG_TAG,"SHIT FUCK= " (Bitmap)intent.getExtras().get("data"));
+                LOG.e(LOG_TAG,"SHIT FUCK= " + (Bitmap)intent.getExtras().get("data"));
                 thumbNailBitmap = (Bitmap)intent.getExtras().get("data");
                 OutputStream os = this.cordova.getActivity().getContentResolver().openOutputStream(uri);
                 bitmap.compress(Bitmap.CompressFormat.JPEG, this.mQuality, os);
