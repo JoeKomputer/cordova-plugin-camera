75cfdec713de1fdf6e80e699313556112108c42c
Josef Kerkinni <jkerkinni@administorsair2.hs.internal>
2015-01-26 16:53:06 -0600

    no message

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -575,7 +575,7 @@ private String ouputModifiedBitmap(Bitmap bitmap, Uri uri) throws IOException {
     public Uri getImageUri(Context inContext, Bitmap inImage) {
       ByteArrayOutputStream bytes = new ByteArrayOutputStream();
       inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-      String path = android.provider.MediaStore.Images.Media.insertImage(inContext, inImage, "Title", null);
+      String path = android.provider.MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
       return Uri.parse(path);
     } 
     /**
