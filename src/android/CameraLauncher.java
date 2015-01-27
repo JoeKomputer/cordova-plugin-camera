765c186fc03f635b23c7401ab33cc78d5bb0af94
Josef Kerkinni <jkerkinni@administorsair2.hs.internal>
2015-01-27 16:19:16 -0600

    no message

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -596,7 +596,8 @@ private String ouputModifiedBitmap(Bitmap bitmap, Uri uri) throws IOException {
       if (resultCode == Activity.RESULT_OK) {
         Uri thumbUri = intent.getData();
         Log.e(LOG_TAG, "BITMAP OF THUMB=" + thumbUri);
-        Bitmap thumbImage = ThumbnailUtils.extractThumbnail(BitmapFactory.decodeFile(thumbUri.toString()), 100, 100);
+       
+        Bitmap thumbImage = android.provider.MediaStore.Images.Thumbnails.getThumbnail(this.cordova.getActivity().getApplicationContext().getContentResolver(), croppedUri,android.provider.MediaStore.Images.Thumbnails.MICRO_KIND,(BitmapFactory.Options) null);
         Log.e(LOG_TAG, "BITMAP OF THUMB=" + thumbImage);
         Uri thumbNailUri = getImageUri(this.cordova.getActivity().getApplicationContext(), thumbImage);
         Log.e(LOG_TAG, "thumb URI HERE=" + thumbNailUri);
